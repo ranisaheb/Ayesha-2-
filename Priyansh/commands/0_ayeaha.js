cbe564570881f5125da83ea2c9891bd4c2403f8d
ranisaheb <ranisaheb61@gmail.com>
2025-02-01 12:09:20 +0530

    Update and rename 0_JULMI.js to 0_ayeaha.js

--- Priyansh/commands/0_ayeaha.js
+++ Priyansh/commands/0_ayeaha.js
@@ -16,12 +16,12 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	if(react.includes("जुल्मी जाट") ||
      react.includes("Boss") || 
      react.includes("BOSS") || 
-     react.includes("ADMIN") ||
+     react.includes("ayeaha") ||
      react.includes("admin") || 
 react.includes("Admin")) {
 		var msg = {
-				body: "★𝗢𝘄𝗻𝗲𝗿ﮩ٨ـﮩ💚💖ـ٨\n\n✦🌸===『*★🌸◉❖जुल्मी≛जाट❖◉✦\n\n★★᭄𝐈𝐍𝐒𝐓𝐀𝐆𝐑𝐀𝐌 𝐋𝐈𝐍𝐊 𝐌𝐄𝐑𝐄 𝐁𝐎𝐒𝐒 𝐊𝐀 :\n\n✦ https://instagram.com/_julmi_jaat__?igshid=OGQ5ZDc2ODk2ZA==  ✦ \n𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐋𝐈𝐍𝐊 𝐌𝐄𝐑𝐄 𝐁𝐎𝐒𝐒 𝐊𝐀😁😋 https://www.facebook.com/julmijaat1008?mibextid=ZbWKwL`",
-				attachment: fs.createReadStream(__dirname + `/noprefix/Julmi.jpg`)
+				body: "★𝗢𝘄𝗻𝗲𝗿ﮩ٨ـﮩ💚💖ـ٨\n\n✦🌸===『*★🌸◉❖𝐀𝐘𝐄𝐀𝐇𝐀≛𝐊𝐇𝐀𝐍❖◉✦\n\n★★᭄\n𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐋𝐈𝐍𝐊 𝐌𝐄𝐑𝐈 𝐁𝐎𝐒𝐒 𝐊𝐀😁�🥰https://www.facebook.com/profile.php?id=61550268698294&mibextid=ZbWKwL`",
+				attachment: fs.createReadStream(__dirname + `/noprefix/8e2558ff8750da7e5d22e519cca16380.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("📷", event.messageID, (err) => {}, true)
