d7695c3c71eba9ac687b56e017a3b8c8399ac995
ranisaheb <ranisaheb61@gmail.com>
2025-02-01 18:22:48 +0530

    Update 0_welcme.js

--- Priyansh/commands/0_welcme.js
+++ Priyansh/commands/0_welcme.js
@@ -13,7 +13,7 @@ module.exports.handleEvent = function({ api, event }) {
   var { threadID, messageID } = event;
   if (event.body.indexOf("wellcome")==0 || (event.body.indexOf("@")==0 || (event.body.indexOf("wlcm")==0 || (event.body.indexOf("welcome")==0)))) {
     var msg = {
-        body: "✰─━━◉❖𝗝𝗨𝗟𝗠𝗜≛𝗝𝗔𝗔𝗧❖◉━━─\n─━━◉❖ 𝐰𝐞𝐥𝐥𝐜𝐨𝐦𝐞 𝐡𝐨 𝐠𝐲𝐚 𝐣𝐢🤗❖◉━━─  ❥═≛🥰 𝐮𝐦𝐦𝐚 😘 𝐮𝐦𝐦𝐚💋≛═❥  ─━━◉❖𝐭𝐡𝐚 💘 𝐭𝐡𝐚 👻𝐤𝐚𝐥 𝐤🙌❖◉━━─✺\n◉❖जुल्मी≛जाट❖◉:",
+        body: "✰─━━◉❖𝐀𝐘𝐄𝐒𝐇𝐀≛𝐊𝐇𝐀𝐍❖◉━━─\n─━━◉❖ 𝐰𝐞𝐥𝐥𝐜𝐨𝐦𝐞 𝐡𝐨 𝐠𝐲𝐚 𝐣𝐢🤗❖◉━━─  ❥═≛🥰 𝐮𝐦𝐦𝐚 😘 𝐮𝐦𝐦𝐚💋≛═❥  ─━━◉❖𝐭𝐡𝐚 💘 𝐭𝐡𝐚 👻𝐤𝐚𝐥 𝐤🙌❖◉━━─✺\n◉❖𝐚𝐲𝐞𝐬𝐡𝐚≛𝐤𝐡𝐚𝐧❖◉:",
         attachment: fs.createReadStream(__dirname + `/noprefix/welcome.mp4`)
       }
       return api.sendMessage(msg, threadID, messageID);
