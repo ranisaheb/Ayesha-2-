99d4aec0cf964d570f92e4c81b404d1359d9cd5c
ranisaheb <ranisaheb61@gmail.com>
2025-02-01 11:52:08 +0530

    Update leavenoti.js

--- Priyansh/events/leavenoti.js
+++ Priyansh/events/leavenoti.js
@@ -20,15 +20,15 @@ module.exports.run = async function({ api, event, Users, Threads }) {
 	const { threadID } = event;
 	const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
 	const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
-	const type = (event.author == event.logMessageData.leftParticipantFbId) ? "खुद ही भाग गया 😐👈" : "एडमिन ने गुस्से में निकाल दिया 😐👈";
-  (typeof data.customLeave == "undefined") ? msg = "सुकर है एक ठरकी इस ग्रुप से कम हो गया 😌👈\nनाम 👉 {name}\nरीजन 👉  {type}" : msg = data.customLeave;
+	const type = (event.author == event.logMessageData.leftParticipantFbId) ? "𝐊𝐇𝐔𝐃 𝐇𝐈 𝐁𝐇𝐀𝐆 𝐆𝐘𝐀 😐👈" : "𝐀𝐃𝐌𝐈𝐍 𝐍𝐄 𝐆𝐔𝐒𝐒𝐄 𝐌𝐄 𝐍𝐈𝐊𝐀𝐋 𝐃𝐈𝐘𝐀😐👈";
+  (typeof data.customLeave == "undefined") ? msg = "𝐒𝐔𝐊𝐀𝐑 𝐇𝐀𝐈 𝐄𝐊 𝐓𝐇𝐀𝐑𝐊𝐈 𝐊𝐀𝐌 𝐇𝐎 𝐆𝐘𝐀  😌👈\nनाम 👉 {name}\nरीजन 👉  {type}" : msg = data.customLeave;
 	msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type);
   
   var link = [  
-"https://i.imgur.com/2hr28Gw.jpg",
-"https://i.imgur.com/2hr28Gw.jpg",
-"https://i.imgur.com/2hr28Gw.jpg",
-"https://i.imgur.com/2hr28Gw.jpg",
+"https://i.imgur.com/gr36kVq.jpeg",
+"https://i.imgur.com/SmCHxJX.jpeg",
+"https://i.imgur.com/sKFY4Td.jpeg",
+"https://i.imgur.com/vksHZHe.jpeg",
   ];
   var callback = () => api.sendMessage({ body: msg, attachment: fs.createReadStream(__dirname + "/cache/leiamnashO.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/leiamnashO.jpg"));
     return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname + "/cache/leiamnashO.jpg")).on("close", () => callback());
