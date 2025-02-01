b2756cfaf9e84a666cfc816b486214d903b4ac2e
ranisaheb <ranisaheb61@gmail.com>
2025-02-01 22:40:48 +0530

    Update Pasta.js

--- Priyansh/commands/Pasta.js
+++ Priyansh/commands/Pasta.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("PASTA")==0 || event.body.indexOf("Pasta")==0 || event.body.indexOf("pasta")==0 || event.body.indexOf(".pasta")==0) {
 		var msg = {
-				body: "💝 YE LO BBY PASTA KHAO 🥐\n\n(─━━◉❖𝗝𝗨𝗟𝗠𝗜≛𝗝𝗔𝗔𝗧❖◉━━─)",
+				body: "💝 YE LO BBY PASTA KHAO 🥐\n\n(─━━◉❖𝐀𝐘𝐄𝐒𝐇𝐀≛𝐊𝐇𝐀𝐍❖◉━━─)",
 				attachment: 
 fs.createReadStream(__dirname + `/cache/Pasta.jpg`)
 			}
